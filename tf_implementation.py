903a462f93479c9e7ea5e284254e81a0e9772fbd
KingHim1 <kingcheung0@gmail.com>
2018-02-14 11:40:47 +0000

    fixed matrix counts

--- tf_implementation.py
+++ tf_implementation.py
@@ -9,6 +9,17 @@ for i in range(1, 9):
 
 stopWords = []
 
+def checkWordCountInText(text, keyWord):
+    splitUpText = text.split(" ")
+    dictionaryOfWords = {}
+    for word in splitUpText:
+        if word in dictionaryOfWords:
+            dictionaryOfWords[word] += 1
+        else:
+            dictionaryOfWords[word] = 1
+    if keyWord in dictionaryOfWords:
+        print("this is the number of keyword: " + keyWord + " =" + dictionaryOfWords[keyWord].__str__())
+
 def countWords(allTexts, stopWords):
     wordCountMatrix = np.array([])
     wordArray = []
@@ -28,13 +39,17 @@ def countWords(allTexts, stopWords):
                 else:
                     wordArray.append(word)
                     columnVec = np.append(columnVec, [1], axis=0)
-        wordCountMatrix.resize((textInd, columnVec.size))
-        wordCountMatrix = np.concatenate((wordCountMatrix, [columnVec]))
+        if np.size(wordCountMatrix, 0) != 0:
+            wordCountMatrix = np.pad(wordCountMatrix, ((0,0),(0,columnVec.size - np.size(wordCountMatrix,1))), 'constant')
+        else:
+            wordCountMatrix.resize((textInd, columnVec.size))
+        wordCountMatrix = np.concatenate((wordCountMatrix, [columnVec]), axis=0)
     return((wordArray, wordCountMatrix, textSizes))
 
 
 
 def calcSentenceWeights(allTexts, wordArray, wordCountMatrix, textSizes):
+    print(wordArray)
     sentenceWeightMatrix = np.array([])
     allSentences = []
     for textInd in range(0, len(allTexts)):
@@ -46,14 +61,19 @@ def calcSentenceWeights(allTexts, wordArray, wordCountMatrix, textSizes):
             words = sentence.split(" ")
             for word in words:
                 if word in wordArray:
-                    sentenceWeights[sentenceInd] += wordCountMatrix[textInd][wordArray.index(word)]
-            if sentenceWeights.size >= np.size(sentenceWeightMatrix, -1):
-                sentenceWeightMatrix.resize((textInd, sentenceWeights.size))
-                sentenceWeightMatrix = np.concatenate((sentenceWeightMatrix, [sentenceWeights]))
+                    # checkWordCountInText(allTexts[textInd], word)
+                    # print(wordCountMatrix[textInd,wordArray.index(word)])
+                    sentenceWeights[sentenceInd] += wordCountMatrix[textInd,wordArray.index(word)]
+        if sentenceWeights.size >= np.size(sentenceWeightMatrix, -1):
+            if np.size(sentenceWeightMatrix, 0)!= 0:
+                sentenceWeightMatrix = np.pad(sentenceWeightMatrix, ((0,0),(0,sentenceWeights.size - np.size(sentenceWeightMatrix, 1))), 'constant')
             else:
-                sentenceWeights.resize(np.size(sentenceWeightMatrix, -1))
-                sentenceWeights = sentenceWeights.flatten()
-                sentenceWeightMatrix = np.concatenate((sentenceWeightMatrix, [sentenceWeights]))
+                sentenceWeightMatrix.resize((textInd, sentenceWeights.size))
+            sentenceWeightMatrix = np.concatenate((sentenceWeightMatrix, [sentenceWeights]), axis=0)
+        else:
+            sentenceWeights.resize(np.size(sentenceWeightMatrix, -1))
+            sentenceWeightMatrix = np.concatenate((sentenceWeightMatrix, [sentenceWeights]), axis=0)
+    # print(allSentences)
     print(sentenceWeightMatrix)
 
 
@@ -62,3 +82,5 @@ listOfWords = countedWords[0]
 wordCountMatr = countedWords[1]
 docSizes = countedWords[2]
 calcSentenceWeights(textTuple, listOfWords, wordCountMatr, docSizes)
+
+

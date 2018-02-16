089630f15752a77179444ad63a61560921cb56cf
KingHim1 <kingcheung0@gmail.com>
2018-02-16 01:37:42 +0000

    added write to summaryfile

--- tf_implementation.py
+++ tf_implementation.py
@@ -7,7 +7,9 @@ for i in range(1, 9):
     text = open("./Test Files/Doc " +  i.__str__() + ".txt", encoding="ISO_8859_1")
     textTuple.append(text.read())
 
-stopWords = []
+stopWords = ["and", "the", "then"]
+
+summaryFile = open("./summary.txt", "w")
 
 def checkWordCountInText(text, keyWord):
     splitUpText = text.split(" ")
@@ -29,8 +31,7 @@ def countWords(allTexts, stopWords):
         columnVec = np.zeros(x)
         #need preprocessing here!
 
-
-        preprocessedText = allTexts[textInd].replace("\n", "").split(" ")
+        preprocessedText = allTexts[textInd].replace("\n", "").replace("\"", "").replace("\n\n", "").split(" ")
         textSizes.append(len(preprocessedText))
         for word in preprocessedText:
             if word not in stopWords:
@@ -39,6 +40,7 @@ def countWords(allTexts, stopWords):
                 else:
                     wordArray.append(word)
                     columnVec = np.append(columnVec, [1], axis=0)
+        columnVec /= textSizes[textInd]
         if np.size(wordCountMatrix, 0) != 0:
             wordCountMatrix = np.pad(wordCountMatrix, ((0,0),(0,columnVec.size - np.size(wordCountMatrix,1))), 'constant')
         else:
@@ -49,7 +51,6 @@ def countWords(allTexts, stopWords):
 
 
 def calcSentenceWeights(allTexts, wordArray, wordCountMatrix, textSizes):
-    print(wordArray)
     sentenceWeightMatrix = np.array([])
     allSentences = []
     for textInd in range(0, len(allTexts)):
@@ -61,8 +62,6 @@ def calcSentenceWeights(allTexts, wordArray, wordCountMatrix, textSizes):
             words = sentence.split(" ")
             for word in words:
                 if word in wordArray:
-                    # checkWordCountInText(allTexts[textInd], word)
-                    # print(wordCountMatrix[textInd,wordArray.index(word)])
                     sentenceWeights[sentenceInd] += wordCountMatrix[textInd,wordArray.index(word)]
         if sentenceWeights.size >= np.size(sentenceWeightMatrix, -1):
             if np.size(sentenceWeightMatrix, 0)!= 0:
@@ -73,14 +72,42 @@ def calcSentenceWeights(allTexts, wordArray, wordCountMatrix, textSizes):
         else:
             sentenceWeights.resize(np.size(sentenceWeightMatrix, -1))
             sentenceWeightMatrix = np.concatenate((sentenceWeightMatrix, [sentenceWeights]), axis=0)
+    return(sentenceWeightMatrix, allSentences)
     # print(allSentences)
     print(sentenceWeightMatrix)
 
 
-countedWords = countWords(textTuple, [])
-listOfWords = countedWords[0]
-wordCountMatr = countedWords[1]
-docSizes = countedWords[2]
-calcSentenceWeights(textTuple, listOfWords, wordCountMatr, docSizes)
+def summarise(allTexts, stopWords, summaryLength):
+    countedWords = countWords(textTuple, stopWords)
+    listOfWords = countedWords[0]
+    wordCountMatr = countedWords[1]
+    docSizes = countedWords[2]
+    sentenceWeightsAndSentences = calcSentenceWeights(textTuple, listOfWords, wordCountMatr, docSizes)
+    sentenceWeightMatrix = sentenceWeightsAndSentences[0]
+    sentences = sentenceWeightsAndSentences[1]
+    summary = []
+    lengthCheckList = []
+    while (len(summary) != 8 or lengthCheckList.count(True) != len(lengthCheckList)):
+        for x in range(0, 8):
+            bestSentence = (sentences[x][np.argmax(sentenceWeightMatrix[x])])
+            sentences[x].remove(bestSentence)
+            allTexts[x] = ".".join(sentences[x])
+            lenOfBestSentence = len(bestSentence.split(" "))
+            if len(summary) != 8 and lenOfBestSentence <= summaryLength:
+                summary.append(bestSentence)
+                lengthCheckList.append(False)
+            elif (lenOfBestSentence + len(summary[x].split(" ")) <= summaryLength) and lenOfBestSentence != 0:
+                summary[x] = summary[x] + bestSentence
+            elif (lenOfBestSentence + len(summary[x].split(" ")) > summaryLength):
+                lengthCheckList[x] = True
+        countedWords = countWords(allTexts, stopWords)
+        listOfWords = countedWords[0]
+        wordCountMatr = countedWords[1]
+        docSizes = countedWords[2]
+        sentenceWeightsAndSentences = calcSentenceWeights(textTuple, listOfWords, wordCountMatr, docSizes)
+        sentenceWeightMatrix = sentenceWeightsAndSentences[0]
+        sentences = sentenceWeightsAndSentences[1]
+    for summaryText in summary:
+        summaryFile.write(summaryText + "\n ---------------------------------------- \n")
 
-
+summarise(textTuple, ["and", "the", "was", "it's"], 150)
